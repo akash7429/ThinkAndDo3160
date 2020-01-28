69c5bccaa0e65fa4279a256999775c2833f0239c
Fariha Penon <farihapenon98@gmail.com>
2020-01-27 17:54:35 -0800

    Little modification in homepage

--- app/src/main/java/com/example/loginauth/HomeActivity.java
+++ app/src/main/java/com/example/loginauth/HomeActivity.java
@@ -1,26 +1,59 @@
 package com.example.loginauth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar_title)
+    TextView toolbarTitle;
+    @BindView(R.id.search2)
+    ImageView search2;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.framelayout)
+    FrameLayout framelayout;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        bottomNavigation.inflateMenu(R.menu.bottom_navigation_main);
+        bottomNavigation.setItemBackgroundResource(R.color.colorPrimary);
+        bottomNavigation.setItemTextColor(ContextCompat.getColorStateList(bottomNavigation.getContext(), R.color.nav_item_colors));
+        bottomNavigation.setItemIconTintList(ContextCompat.getColorStateList(bottomNavigation.getContext(), R.color.nav_item_colors));
+
+
     }
 
-    public void logout(View view){
+    public void logout(View view) {
 
         FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
         //not finished();
     }
